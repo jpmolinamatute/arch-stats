ea08a6d3401f0b7cc63ce79abe36281aab5772f5
Juan Pablo Molina Matute <jpmolinamatute@gmail.com>
2024-12-26 17:57:29 -0500

    added a simple create_uuid script

--- docs/diagrams_generator.py
+++ docs/diagrams_generator.py
@@ -5,29 +5,6 @@ from pathlib import Path
 from graphviz import Digraph
 
 
-def lane_setup(**opt: Mapping[str, Any]) -> None:
-    dot = Digraph(
-        comment="Shooting flow.",
-        node_attr={"color": "yellow"},
-        edge_attr={"color": "yellow"},
-        graph_attr={
-            "label": "Shooting flow.",
-            "labelloc": "t",
-            "labeljust": "c",
-            "fontsize": "20",
-        },
-    )
-    dot.node(name="set_lane", label="Set a lane for the tournament.", shape="box")
-    dot.node(name="get_target_hight", label="Measure target height", shape="box", style="rounded")
-    dot.node(name="get_lane_length", label="Measure lane length", shape="box", style="rounded")
-    dot.node(
-        name="get_x_rings", label="Measure X coord\nfor each ring", shape="box", style="rounded"
-    )
-    dot.node("start", shape="circle", label="Start")
-    dot.node("end", shape="doublecircle", label="End")
-    dot.render("shooting_flow", **opt)
-
-
 def shooting_flow(**opt: Mapping[str, Any]) -> None:
     dot = Digraph(
         comment="Shooting flow.",
@@ -45,50 +22,79 @@ def shooting_flow(**opt: Mapping[str, Any]) -> None:
     dot.render("shooting_flow", **opt)
 
 
-def archer_tournament_creation(**opt: Mapping[str, Any]) -> None:
+def tournament_creation(**opt: Mapping[str, Any]) -> None:
     dot = Digraph(
-        comment="Archer creation flow.",
-        node_attr={"color": "blue"},
-        edge_attr={"color": "blue"},
+        comment="Tournament creation flow.",
         graph_attr={
-            "label": "Archer creation flow.",
+            "label": "Tournament creation flow.",
             "labelloc": "t",
             "labeljust": "c",
             "fontsize": "20",
         },
     )
-    dot.node("archer_start", shape="circle", label="Start")
-    dot.node("tournament_start", shape="circle", label="Start")
-    dot.node(name="archer_exist", label="Does the archer exist?", shape="diamond")
-    dot.node(name="exist_yes", label="Yes.")
-    dot.node(name="archer_exist_no", label="No.")
-    dot.node(name="ready", label="Ready to register\nin a tournament.", shape="box")
-    dot.node(name="collect_info", label="Collect archer's info.", shape="box")
-    dot.node(name="collect_arrow_info", label="Collect archer's\narrows info.", shape="box")
-    dot.node(name="tournament_exist", label="Is the tournament created?", shape="diamond")
-    dot.node(name="tournament_exist_no", label="No.")
-    dot.node(name="create_tournament", label="Create a tournament", shape="box")
-    dot.node(name="ready", label="Ready to register\nin a tournament.", shape="box")
+    dot.node("creation_starts", shape="circle", label="Start", color="blue")
+    dot.node("is_there_tournament", shape="diamond", label="Is there a tournament?")
+    dot.node("is_there_tournament_yes", label="Yes.")
+    dot.node("is_there_tournament_no", label="No.")
+    dot.node("create_tournament", shape="box", label="Create tournament")
+    dot.node("setup_lane", shape="box", label="Setup lane")
+    dot.node("setup_target", shape="box", label="Setup target")
+    dot.node("does_archer_exist", shape="diamond", label="Does archer exist?")
+    dot.node("does_archer_exist_yes", label="Yes.")
+    dot.node("does_archer_exist_no", label="No.")
+    dot.node("is_archer_registered", shape="diamond", label="Is archer registered?")
+    dot.node("is_archer_registered_yes", label="Yes.")
+    dot.node("is_archer_registered_no", label="No.")
+    dot.node("register_archer", shape="box", label="Register archer")
+    dot.node("is_there_more_archers", shape="diamond", label="Is there more archers?")
+    dot.node("is_there_more_archers_yes", label="Yes.")
+    dot.node("is_there_more_archers_no", label="No.")
+    dot.node("create_archer", shape="box", label="Create archer")
+    dot.node("collect_archer_info", shape="box", label="Collect archer info")
+    dot.node("collect_arrow_info", shape="box", label="Collect arrow info")
+    dot.node("creation_ends", shape="doublecircle", label="End", color="red")
 
-    dot.edge("archer_start", "archer_exist")
-    dot.edges([("archer_exist", "exist_yes"), ("archer_exist", "archer_exist_no")])
-    dot.edge("archer_exist_no", "collect_info")
-    dot.edges([("collect_info", "ready"), ("collect_info", "collect_arrow_info")])
-    dot.edge("exist_yes", "ready")
-
-    dot.edge("tournament_start", "tournament_exist")
+    dot.edge("creation_starts", "is_there_tournament")
     dot.edges(
         [
-            ("tournament_exist", "exist_yes"),
-            ("tournament_exist", "tournament_exist_no"),
+            ("is_there_tournament", "is_there_tournament_yes"),
+            ("is_there_tournament", "is_there_tournament_no"),
         ]
     )
-    dot.edge("tournament_exist_no", "create_tournament")
-
-    dot.edge("create_tournament", "ready")
-
-    dot.edge("ready", "end")
-    dot.render("archer_creation", **opt)
+    dot.edge("is_there_tournament_yes", "does_archer_exist")
+    dot.edge("is_there_tournament_no", "create_tournament")
+    dot.edge("create_tournament", "setup_lane")
+    dot.edge("setup_lane", "setup_target")
+    dot.edge("setup_target", "does_archer_exist")
+    dot.edges(
+        [
+            ("does_archer_exist", "does_archer_exist_yes"),
+            ("does_archer_exist", "does_archer_exist_no"),
+        ]
+    )
+    dot.edge("does_archer_exist_yes", "is_archer_registered")
+    dot.edge("does_archer_exist_no", "create_archer")
+    dot.edges(
+        [
+            ("is_archer_registered", "is_archer_registered_yes"),
+            ("is_archer_registered", "is_archer_registered_no"),
+        ]
+    )
+    dot.edge("is_archer_registered_no", "register_archer")
+    dot.edge("create_archer", "collect_archer_info")
+    dot.edge("collect_archer_info", "collect_arrow_info")
+    dot.edge("collect_arrow_info", "register_archer")
+    dot.edge("register_archer", "is_there_more_archers")
+    dot.edges(
+        [
+            ("is_there_more_archers", "is_there_more_archers_yes"),
+            ("is_there_more_archers", "is_there_more_archers_no"),
+        ]
+    )
+    dot.edge("is_there_more_archers_yes", "setup_lane")
+    dot.edge("is_there_more_archers_no", "creation_ends")
+    dot.edge("is_archer_registered_yes", "creation_ends")
+    dot.render("tournament_creation", **opt)
 
 
 def main() -> None:
@@ -101,7 +107,7 @@ def main() -> None:
         "directory": docs_path,
     }
 
-    archer_tournament_creation(**opt)
+    tournament_creation(**opt)
 
 
 if __name__ == "__main__":
