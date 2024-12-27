12d8a819a1fb57ca9a076807b8d7bfd6203b2794
Juan Pablo Molina Matute <jpmolinamatute@users.noreply.github.com>
2024-12-27 12:17:45 -0500

    serving the angular app (#2)

--- backend/server/src/main.rs
+++ backend/server/src/main.rs
@@ -1,13 +1,31 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+extern crate dotenv;
 
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+use actix_files as fs;
+use actix_web::{App, HttpServer};
+use dotenv::dotenv;
+use std::env;
+use std::path::Path;
+
+fn get_webui_path() -> String {
+    let webui_path = env::var("ARCH_STATS_WEBUI_PATH").expect("ARCH_STATS_WEBUI_PATH must be set");
+    if Path::new(&webui_path).is_dir() {
+        webui_path
+    } else {
+        panic!(
+            "The path specified in 
+         does not exist or is not a directory"
+        );
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(index)))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    dotenv().ok();
+    let webui_path = get_webui_path();
+    HttpServer::new(move || {
+        App::new().service(fs::Files::new("/", &webui_path).index_file("index.html"))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
