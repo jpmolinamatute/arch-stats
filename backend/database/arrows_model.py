1eab71f96e0832432acccc4be5ec0de8ee509ac5
Juan Pablo Molina Matute <jpmolinamatute@gmail.com>
2025-05-15 17:20:15 -0600

    implemented targets sessions model

--- backend/database/arrows_model.py
+++ backend/database/arrows_model.py
@@ -8,13 +8,13 @@ from database.base_model import Base
 # pylint: disable=too-few-public-methods
 
 
-class Arrow(Base):
+class Arrows(Base):
     __tablename__ = "arrows"
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     human_identifier: Mapped[str] = mapped_column(String(10), nullable=False, unique=True)
-    weight: Mapped[float] = mapped_column(Float, nullable=True)
-    diameter: Mapped[float] = mapped_column(Float, nullable=True)
-    spine: Mapped[float] = mapped_column(Float, nullable=True)
-    length: Mapped[float] = mapped_column(Float, nullable=True)
-    label_position: Mapped[float] = mapped_column(Float, nullable=True)
+    weight: Mapped[float | None] = mapped_column(Float, nullable=True)
+    diameter: Mapped[float | None] = mapped_column(Float, nullable=True)
+    spine: Mapped[float | None] = mapped_column(Float, nullable=True)
+    length: Mapped[float | None] = mapped_column(Float, nullable=True)
+    label_position: Mapped[float | None] = mapped_column(Float, nullable=True)
