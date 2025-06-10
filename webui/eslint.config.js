182be822531a3ffb46f5f81ea5f6cb250893f989
Juan Pablo Molina Matute <jpmolinamatute@users.noreply.github.com>
2025-06-10 13:59:26 -0600

    improving development mode (#81)

--- webui/eslint.config.js
+++ webui/eslint.config.js
@@ -1,16 +1,40 @@
 import js from '@eslint/js';
-import prettier from 'eslint-config-prettier';
-import tseslint from 'typescript-eslint';
+import tsParser from '@typescript-eslint/parser';
+import prettierConfig from 'eslint-config-prettier';
+import eslint from '@eslint/js';
 import prettierPlugin from 'eslint-plugin-prettier';
+import tseslint from 'typescript-eslint';
 
-export default [
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
     js.configs.recommended,
-    ...tseslint.configs.recommended,
+    prettierConfig,
     {
-        plugins: { prettier: prettierPlugin },
+        files: ['./**/*.{js,jsx,ts,tsx}'],
+        ignores: ['./eslint.config.js', './vite.config.js'],
         rules: {
+            semi: 'error',
+            'prefer-const': 'error',
             'prettier/prettier': 'error',
         },
+        plugins: {
+            prettier: prettierPlugin,
+        },
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: 2022,
+                sourceType: 'module',
+                project: './tsconfig.json',
+            },
+            globals: {
+                document: 'readonly',
+                window: 'readonly',
+                console: 'readonly',
+                fetch: 'readonly',
+                location: 'readonly',
+            },
+        },
     },
-    prettier,
-];
+);
