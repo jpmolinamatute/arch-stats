4b0c1d692044d57de7db6ef4785fb1455ca88203
Juan Pablo Molina Matute <jpmolinamatute@gmail.com>
2025-08-14 18:32:48 -0600

    reverting FE changes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
     import { computed } from 'vue';
     import { uiManagerStore } from './state/uiManagerStore';
-    // @ts-expect-error TS6133
     import { sessionOpened } from './state/session';
     import type { ViewName } from './state/uiManagerStore';
 
+    import NewArrow from './components/forms/NewArrow.vue';
+    import WizardSession from './components/WizardSession.vue';
+    import CalibrateTarget from './components/forms/CalibrateTarget.vue';
+    import ShotsTable from './components/ShotsTable.vue';
+    import { closeSession } from './composables/useSession';
+
     // Map view names to components
-    // @ts-ignore TS2739
-    const componentsMap: Record<ViewName, any> = {};
+    const componentsMap: Record<ViewName, any> = {
+        ArrowForm: NewArrow,
+        SessionForm: WizardSession,
+        TargetForm: CalibrateTarget,
+        ShotsTable: ShotsTable,
+    };
 
     // Compute the active component to render
     const currentComponent = computed(() => {
@@ -18,10 +27,26 @@
     function handleSubmit() {
         uiManagerStore.clearView();
     }
+    const buttonText = computed(() => {
+        return sessionOpened.is_opened === true ? 'Close Session' : 'Open Session';
+    });
+
+    function handleClick() {
+        if (sessionOpened.is_opened === true) {
+            closeSession();
+        } else {
+            uiManagerStore.setView('SessionForm');
+        }
+    }
 </script>
 
 <template>
-    <header></header>
+    <header>
+        <button @click="handleClick()">{{ buttonText }}</button>
+        <button @click="uiManagerStore.setView('ArrowForm')">Register Arrow</button>
+        <button @click="uiManagerStore.setView('TargetForm')">Register Target</button>
+        <button @click="uiManagerStore.setView('ShotsTable')">View Shots</button>
+    </header>
 
     <main>
         <component :is="currentComponent" v-if="currentComponent" @submit="handleSubmit" />
