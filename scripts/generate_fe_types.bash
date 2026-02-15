9eb47ed4caf4dc13d42c7733da160a9f37ea4070
Juan Pablo Molina Matute <jpmolinamatute@gmail.com>
2026-02-15 15:53:47 -0500

    implementing PR feedback

--- scripts/generate_fe_types.bash
+++ scripts/generate_fe_types.bash
@@ -6,33 +6,34 @@ ROOT_DIR="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/.." && pwd)"
 ENV_FILE="${ROOT_DIR}/.env"
 
 openapi_via_script() {
-    local openapi_source
-    openapi_source="$1"
-    cd "${ROOT_DIR}/backend"
-    export PYTHONPATH="${ROOT_DIR}/backend:${ROOT_DIR}/backend/src"
-    uv run ./tools/generate_openapi.py "${openapi_source}"
-    cd "${ROOT_DIR}"
+    local openapi_source="$1"
+    (
+        cd "${ROOT_DIR}/backend"
+        export PYTHONPATH="${ROOT_DIR}/backend:${ROOT_DIR}/backend/src"
+        echo "Info: Generating OpenAPI spec from script"
+        uv run ./tools/generate_openapi.py "${openapi_source}"
+    )
 }
 
 frontend() {
-    local openapi_source
-    openapi_source="$1"
-    cd "${ROOT_DIR}/frontend"
-    npx openapi-typescript "${openapi_source}" --export-type --output src/types/types.generated.ts
-    cd "${ROOT_DIR}"
+    local openapi_source="$1"
+    (
+        cd "${ROOT_DIR}/frontend"
+        echo "Info: Generating frontend types from ${openapi_source}"
+        npx openapi-typescript "${openapi_source}" --export-type --output src/types/types.generated.ts
+    )
 }
 
 main() {
-    local count openapi_source
+    local openapi_source="http://localhost:${ARCH_STATS_SERVER_PORT}/api/openapi.json"
     if [[ ! -f "${ENV_FILE}" ]]; then
         echo "Error: .env file not found at ${ENV_FILE}." >&2
         exit 1
     fi
     # shellcheck source=../.env
     source "${ENV_FILE}"
-    openapi_source="http://localhost:${ARCH_STATS_SERVER_PORT}/api/openapi.json"
-    count=$(pgrep -af uvicorn | wc -l)
-    if [[ "$count" -ne 2 ]]; then
+
+    if ! curl --silent --fail --head "${openapi_source}" > /dev/null 2>&1; then
         openapi_source="${ROOT_DIR}/openapi.json"
         openapi_via_script "${openapi_source}"
     fi
