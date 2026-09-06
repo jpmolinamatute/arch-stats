161605c593bd41ecb9288db165a2e5cfa0968196
Juan Pablo Molina Matute <jpmolinamatute@users.noreply.github.com>
2026-09-05 23:29:12 -0400

    refactor/033 ci build artifact workflow take 2 (#222)
    - **fix(frontend): output build to dist directory for CI embedding**
    - **just for fun**

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig(({ mode }) => {
             {
                 name: 'clean-out-dir',
                 buildStart() {
-                    const outDir = fileURLToPath(new URL('../backend-old/src/frontend', import.meta.url))
+                    const outDir = fileURLToPath(new URL('../backend/frontend', import.meta.url))
                     const files = readdirSync(outDir)
                     for (const file of files) {
                         if (file !== '.gitkeep') {
@@ -35,7 +35,7 @@ export default defineConfig(({ mode }) => {
             },
         },
         build: {
-            outDir: '../backend-old/src/frontend',
+            outDir: '../backend/frontend',
             emptyOutDir: false,
         },
         server: {
